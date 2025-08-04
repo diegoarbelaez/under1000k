2ad03d75751b42004d9808be32c66d8195d709dc
Diego Arbelaez <correo@diegoarbelaez.com>
2025-08-04 10:49:53 +0200

    Add database population commands to build script for production deployment

--- build.sh
+++ build.sh
@@ -5,4 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate 
+python manage.py migrate
+
+# Poblar categorías y datos de ejemplo
+python manage.py populate_categories
+python manage.py populate_sample_data 
